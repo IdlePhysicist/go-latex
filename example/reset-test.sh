f93ffc8f44e6a6c759f3cd26c1c974e5474809ec
Eoghan <idlephysicist@vivaldi.net>
2019-10-13 16:10:30 -0600

    Merge new reg expression into dev branch (#2)
    * Changed reg expression to allow better matching
    
    * updated examples to test new regex

--- example/reset-test.sh
+++ example/reset-test.sh
@@ -3,4 +3,24 @@ if [[ ${PWD##*/} != "example" ]]; then
   cd example
 fi
 
-echo "package test\n\nfunc \\Xi() {\n	return \`\\xi latex is \\pi \\phi\`\n}\n" > test-file.go
+echo 'package test
+
+func \\Xi() {
+  return \`\\xi latex is \\pi \\phi\`
+}
+
+func \\gammaDot() {
+  return \`\\gammaDot`
+}
+
+func Frat() {
+  return "\kappa\gamma\eta"
+
+func main() {
+  \\hbar_star := 0.0562
+  x := \\Xi()
+  y := \\gammaDot()
+
+}
+
+' > test-file.go
